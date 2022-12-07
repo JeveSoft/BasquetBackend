16f90cd88144c4c7ca112fceec6fd29731552dc3
jjevesoft@gmail.com <sergiosoliznogales@gmail.com>
2022-12-06 23:04:43 -0400

    CompletadoPartidos

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Administrador;
 use Illuminate\Http\Request;
 
+
 class AdministradorController extends Controller
 {
     public function obtenerAdministrador($id)
